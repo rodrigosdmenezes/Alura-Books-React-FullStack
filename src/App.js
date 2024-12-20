36cb429ef3bdd1ed5b88e4e0f4d6b52d8526bd62
rodrigosdmenezes <rodrigosdmenezes@gmail.com>
2024-12-20 11:56:22 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './img/logo.svg';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div className='logo'>
+          <img src={logo} alt='img-logo'></img>
+          <p>Alura Books</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
